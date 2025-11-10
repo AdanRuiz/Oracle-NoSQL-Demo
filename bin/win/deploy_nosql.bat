60654aec1479d2eb0849b4330c7802c170399fa1
Adan Ruiz <ardn0001@hotmail.com>
2025-11-10 09:29:04 -0600

    LF

--- bin/win/deploy_nosql.bat
+++ bin/win/deploy_nosql.bat
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker cp ../../sources/ oracle-nosql:/u02/setup
-docker exec -it oracle-nosql bash -c 'chown -R ons:ons /u02/setup'
-docker exec -it oracle-nosql bash -c '(cd /u02/setup/sources/;chmod +x *.sh)'
+docker exec -it oracle-nosql bash -c "chown -R ons:ons /u02/setup"
+docker exec -it oracle-nosql bash -c "(cd /u02/setup/sources/;chmod +x *.sh)"
 #docker exec -it oracle-nosql sudo -u ons /u02/setup/sources/nosql_install.sh
 docker exec -it oracle-nosql sudo runuser -l ons -c /u02/setup/sources/nosql_install.sh
 docker exec -it oracle-nosql bash
